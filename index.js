a67e307cb7c34e3e9999049255f97f752990eb1e
spynter <spynter123@gmail.com>
2022-03-08 19:59:40 -0400

    add files

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
-// settings
-app.set('port', 3000);
-app.set('view engine', 'ejs');
+const port = process.env.PORT || 4000;
 
-// middlewares
+app.listen(port);
 
-
-// routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'views/index.html'));
-});
-
-// static files
-
-// listening the server
-app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
-});
+console.log(`Escuchando en el puerto ${port}`);
